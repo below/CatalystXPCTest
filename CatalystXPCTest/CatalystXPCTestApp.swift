255054dd5e9ebd7a6aaa81750a6efbf298507240
Alexander von Below <Alex@vonBelow.Com>
2021-11-27 23:08:57 +0100

    Initial Commit

---
+++ CatalystXPCTest/CatalystXPCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalystXPCTestApp.swift
+//  CatalystXPCTest
+//
+//  Created by Alexander v. Below on 27.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CatalystXPCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
